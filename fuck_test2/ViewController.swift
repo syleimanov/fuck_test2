21b8eaca192e7971cbc9e76dd85053982f032195
VLADIMIR SYLEIMANOV <bannabels@gmail.com>
2021-01-12 20:24:19 +0300

    Initial Commit

---
+++ fuck_test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fuck_test2
+//
+//  Created by Vladimir Syleimanov on 12.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
